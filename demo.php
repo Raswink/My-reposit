fe88e50f408b2c73a4ca264efb09a0c53eb19f06
rashik <rashikmah7@gmail.com>
2017-08-20 08:51:19 +0545

    demo file include basic lesson of php

---
+++ demo.php
@@ -0,0 +1,77 @@
+<?php
+
+// $x = 10;
+// $y = 5;
+
+// echo $x+$y . "<br>" ;
+
+// echo 10 - 5;
+
+// $string = 'hello world';
+
+// echo strlen($string) . "<br>";
+
+// echo strlen('news paper');
+// echo "<br>";
+
+// echo str_word_count($string);
+// echo "<br>";
+// echo strrev("cosmos college");
+// echo "<br>";
+// echo str_replace('cosmos', 'caribbean', 'cosmos college');
+
+// $person = array(
+// 	'0' => 'honda',
+// 	'1' => 'yamaha',
+// 	'2' => 'dio');
+
+// echo 'i love ' . $person[1] . ' bike';
+
+// $person = array(
+// 	'fname' => 'anis',
+// 	'lname' => 'buda'
+// 	);
+
+// echo 'laxmi + ' . $person['fname'];
+
+// $cars = array(
+// 	'c1' => array(
+// 		'name' => 'suzuki',
+// 		'c_no' => 'Ba 11'
+// 		),
+
+// 	'c2' => array(
+// 		'name' => 'honda',
+// 		'c_no' => 'Ba 75'
+// 		)
+// 	);
+
+// echo $cars['c1']['name'];
+
+$x = 10;
+$y = 5;
+
+// if($x == $y){
+// 	echo 'not lovers';
+// }else{
+// 	echo 'they are lovers';
+// }
+
+// for ($i=0; $i < 5; $i++) { 
+// 	echo $x+$y .'<br>';
+// }
+
+
+// $x = 'sunday';
+// switch ($x) {
+// 	case 'sunday':
+// 		echo 'happy holiday';
+// 		break;
+// 	case 'monday':
+// 		echo 'no holiday';
+// 			break;	
+	
+// 	default:
+// 		echo 'public holiday';
+// 		break;
+// }
